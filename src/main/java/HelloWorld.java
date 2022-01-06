9d0a4dd420ba863fabf1cb9de9d299785d126356
opentlc-mgr <mroberts@redhat.com>
2022-01-06 16:35:46 +0000

    New helloworld

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        String var1 = "Hello";
+        String var2 = "world";        
+        System.out.println(var1 + " " + var2);
+    }
+}
